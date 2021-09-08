47c3a1d248daec45efd2679993ead21e48faea74
Luke Michalakis <michalakisluke@gmail.com>
2021-09-08 18:15:01 -0400

    <Begin building JavaScript>

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var randomDate = '02/23/1999';
+// Convert the `randomDate` to the "MM/DD/YYYY" format using `moment()`
+var convertedDate = moment(randomDate).format('MM/DD/YYYY');
+// Log `convertedDate` into the console
+console.log(convertedDate);
