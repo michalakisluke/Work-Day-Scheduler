15f25ea06870d8e0e7b959eefb7500272779b06c
Luke Michalakis <michalakisluke@gmail.com>
2021-09-09 18:21:39 -0400

    <Include placemarker console logs>

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,22 +13,25 @@ $(".hour").each(function(index, element) {
 $(".hour").each(function(index) {
     console.log("Value at index " + hours[index]);
     console.log("Value at current " + currentTime);
-    console.log("Values are equal");
-    console.log(hours[index] === currentTime);
-    console.log("Current time is earlier than index"); 
-    console.log(hours[index] > currentTime);
-    console.log("Current time is later than index"); 
-    console.log(hours[index] < currentTime);
-    console.log("New time");
+    // console.log("Values are equal");
+    // console.log(hours[index] === currentTime);
+    // console.log("Current time is earlier than index"); 
+    // console.log(hours[index] > currentTime);
+    // console.log("Current time is later than index"); 
+    // console.log(hours[index] < currentTime);
+    // console.log("New time");
     if (hours[index] === currentTime) {
         $(".description").attr("class", "col-10 description present");
+        console.log("equal");
     }
     
     else if (hours[index] > currentTime) {
         $(".description").attr("class", "col-10 description future");
+        console.log("It's future")
     }
     
     else if (hours[index] < currentTime) {
         $(".description").attr("class", "col-10 description past");
+        console.log("It's past")
     };
 });
