fd3940be665db9746d1dfd369dfe0ebd43ac9c55
Luke Michalakis <michalakisluke@gmail.com>
2021-09-09 14:44:56 -0400

    <Build website rows>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,33 @@
-var trackDate = function() {
-    var date = moment().format("dddd, MMMM Do");
-    console.log(date);
-    document.querySelector("#currentDay").textContent = date;
-}
+var date = moment().format("dddd, MMMM Do");
+document.querySelector("#currentDay").textContent = date;
 
-trackDate();
+for (i = 0; i < 24; i++) {
+    //Create rows ---> I think done
+    var row = document.createElement("div");
+    row.setAttribute("class", "row");
+    document.querySelector(".container").appendChild(row);
+
+    // Create time element ---> Not done!
+    var time = document.createElement("div");
+    time.setAttribute("class", "hour");
+    time.textContent = moment().format('LT');
+    row.appendChild(time);
+
+    // create work to do element
+    var work = document.createElement("div");
+    work.setAttribute("class", "time-block");
+    var workText = document.createElement("input");
+    workText.setAttribute("class", "description");
+    work.appendChild(workText);
+    row.appendChild(work);
+
+    // create save button element
+    var save = document.createElement("button");
+    save.setAttribute("class", "saveBtn");
+    var saveText = document.createElement("i");
+    saveText.TextContent = "Save";
+    save.appendChild(saveText);
+    row.appendChild(save);
+
+
+}
