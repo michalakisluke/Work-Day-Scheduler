df3273e21a31d2ccf70607be23bc42249921eb94
Luke Michalakis <michalakisluke@gmail.com>
2021-09-09 18:51:38 -0400

    <Continue troubleshoot of time tracking>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 var date = moment().format("dddd, MMMM Do");
 document.querySelector("#currentDay").textContent = date;
 var hours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
-var currentTime = moment().format("HH");
+//var currentTime = moment().format("HH");
+
+var currentTime = 12;
+
 
 
 $(".hour").each(function(index, element) {
@@ -13,25 +16,20 @@ $(".hour").each(function(index, element) {
 $(".hour").each(function(index) {
     console.log("Value at index " + hours[index]);
     console.log("Value at current " + currentTime);
-    // console.log("Values are equal");
-    // console.log(hours[index] === currentTime);
-    // console.log("Current time is earlier than index"); 
-    // console.log(hours[index] > currentTime);
-    // console.log("Current time is later than index"); 
-    // console.log(hours[index] < currentTime);
-    // console.log("New time");
-    if (hours[index] === currentTime) {
-        $(".description").attr("class", "col-10 description present");
+    console.log(typeof hours[index]);
+    console.log(typeof currentTime);
+    if (parseInt(hours[index]) === parseInt(currentTime)) {
+        $("#hour-"+ index).attr("class", "col-10 description present");
         console.log("equal");
     }
     
-    else if (hours[index] > currentTime) {
-        $(".description").attr("class", "col-10 description future");
+    else if (parseInt(hours[index]) > parseInt(currentTime)) {
+        $("#hour-"+ index).attr("class", "col-10 description future");
         console.log("It's future")
     }
     
-    else if (hours[index] < currentTime) {
-        $(".description").attr("class", "col-10 description past");
+    else if (parseInt(hours[index]) < parseInt(currentTime)) {
+        $("#hour-"+ index).attr("class", "col-10 description past");
         console.log("It's past")
     };
 });
