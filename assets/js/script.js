09a99e32c92feb0b77303330617a98c4afd3f565
Luke Michalakis <michalakisluke@gmail.com>
2021-09-09 18:59:16 -0400

    <Finish adding functionality for time tracking>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,28 @@
 var date = moment().format("dddd, MMMM Do");
 document.querySelector("#currentDay").textContent = date;
 var hours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
-//var currentTime = moment().format("HH");
+var currentTime = moment().format("HH");
 
-var currentTime = 12;
-
-
-
+// Assign Hours to hour element
 $(".hour").each(function(index, element) {
     $(element).text(moment().hour(hours[index]).format("h A"));
 });
 
-
-
+// Check if times have passed
 $(".hour").each(function(index) {
-    console.log("Value at index " + hours[index]);
-    console.log("Value at current " + currentTime);
-    console.log(typeof hours[index]);
-    console.log(typeof currentTime);
+    var idSelect = index + 1;
     if (parseInt(hours[index]) === parseInt(currentTime)) {
-        $("#hour-"+ index).attr("class", "col-10 description present");
+        $("#hour-"+ idSelect).attr("class", "col-10 description present");
         console.log("equal");
     }
     
     else if (parseInt(hours[index]) > parseInt(currentTime)) {
-        $("#hour-"+ index).attr("class", "col-10 description future");
+        $("#hour-"+ idSelect).attr("class", "col-10 description future");
         console.log("It's future")
     }
     
     else if (parseInt(hours[index]) < parseInt(currentTime)) {
-        $("#hour-"+ index).attr("class", "col-10 description past");
+        $("#hour-"+ idSelect).attr("class", "col-10 description past");
         console.log("It's past")
     };
 });
