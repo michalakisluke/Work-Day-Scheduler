518b9695747536a764283cbca29e0be543ff1614
Luke Michalakis <michalakisluke@gmail.com>
2021-09-09 16:36:51 -0400

    <Fix styling>

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,10 @@ document.querySelector("#currentDay").textContent = date;
 var hours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 
 
- $(".hour").each(function(index, element) {
+$(".hour").each(function(index, element) {
     $(element).text(moment().hour(hours[index]).format("h A"));
 });
+
 
 
 // Everything below this point is probably useless
